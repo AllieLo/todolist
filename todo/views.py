6b30bf987b0ad261d7f68546b60976c44ac17c68
allie <adaipoo@gmail.com>
2023-05-22 22:42:05 +0800

    1.完成新增Todo 2.完成檢視改動及更新功能

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
 
 
-# Create your views here.
 def create_todo(request):
     message = ""
     form = TodoForm()
@@ -13,16 +13,34 @@ def create_todo(request):
         todo.user = request.user
         todo.save()
         message = "建立Todo成功"
+        return redirect("todolist")
     return render(request, "todo/create_todo.html", {"form": form, "message": message})
 
 
 def todo(request, id):
     todo = None
+    message = ""
     try:
-        todo = Todo.objects.get(pk=id)
+        if request.method == "GET":
+            # 將取得的todo事項給表單
+            todo = Todo.objects.get(pk=id)
+            form = TodoForm(instance=todo)
+        if request.method == "POST":
+            print(request.POST)
+            form = TodoForm(request.POST, instance=todo)
+            if form.is_valid():
+                todo = form.save(commit=False)
+                if todo.completed:
+                    todo.date_completed = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                todo.save()
+                message = "更新成功"
     except Exception as e:
         print(e)
-    return render(request, "todo/todo.html", {"todo": todo})
+        message = "更新失敗"
+    return render(
+        request, "todo/todo.html", {"todo": todo, "form": form, "message": message}
+    )
 
 
 def todolist(request):
